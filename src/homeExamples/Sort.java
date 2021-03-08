85d9e56cb77431c1e49d82f9b4c505f335a238c8
Aleksandr Kozhenov <kozhenov1984@mail.ru>
2021-03-08 20:23:24 +0400

    fix

--- src/homeExamples/Sort.java
+++ src/homeExamples/Sort.java
@@ -1,4 +1,51 @@
 package homeExamples;
 
 public class Sort {
+    // Сортировка массива пузырьком
+    public static void simpleSort(int[] mus) {
+        for (int i = mus.length-1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (mus[j] > mus[j+1]) {
+                    int temp = mus[j];
+                    mus[j] = mus[j+1];
+                    mus[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // Быстрая сортировка
+    public static void quickSort (int mus[], int low, int high) {
+        if (mus.length == 0) return;
+        if (low >= high) return;
+
+        int middle = low + (high - low)/2;
+        int support = mus[middle];
+        int left = low, right = high;
+
+        while (left < right) {
+            while (mus[left] < support) {
+                left++;
+            }
+            while (mus[right] > support) {
+                right--;
+            }
+
+            if (left <= right) {
+                int temp = mus[left];
+                mus[left] = mus[right];
+                mus[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        if (low < right) {
+            quickSort(mus, low, right);
+        }
+        if (high > left) {
+            quickSort(mus, left, high);
+        }
+    }
+
 }
--- src/homeExamples/WorkArrays.java
+++ src/homeExamples/WorkArrays.java
@@ -1,4 +1,21 @@
 package homeExamples;
 
 public class WorkArrays {
+    // Создать массив на n элементов
+    public static int[] createMus(int n) {
+        int[] mus = new int[n];
+        for (int i = 0; i < mus.length; i++) {
+            mus[i] = (int) (Math.random()*100);
+        }
+        return mus;
+    }
+
+    // Вывод массива на экран
+    public static void printMus(String str, int[] mus) {
+        System.out.println(str);
+        for (int value : mus) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
 }
